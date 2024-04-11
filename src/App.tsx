6b36595458966da8f2b13c10c7afb2624e1ef92b
Pirashan <pirashan99@gmail.com>
2024-04-11 16:04:35 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+<div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4">
+  <h2 className="text-4xl text-yellow-500">
+    Hello World!
+  </h2>
+</div>
+  )
+}
+
+export default App
